f4f4660282f98b9ea2ac58eb9b332e8a6638de80
ras09 <rejeesh09@yahoo.co.in>
2021-01-11 00:58:36 +0530

    just a template for further rounds, no play happens currently in round2

--- round_2.py
+++ round_2.py
@@ -1,10 +1,14 @@
+#########################################################################
 from round_1 import Round_1
-# obj1=Round_1(True)
-    
+# Round_1() class is where the first round of the game takes place(i.e. round1_play() method in Round_1())
+# so the round1_play() method has to be called from Round_2 and round2_play() from Round_3 and so on
+#########################################################################
 class Round_2(Round_1):
-    def __init__(self,hold):
+    def __init__(self,hold,custom_deal):
         self.hold=hold
-        super().__init__(self.hold)
+        self.custom_deal=custom_deal
+        # don't think the arguments passed have to be made self.
+        super().__init__(self.hold,self.custom_deal)
         # kind of creating an object instance of Round_1() class with the above __init__()
         super().round1_play()
         # similarly round2_play(to be defined){as well as __init__()} 
